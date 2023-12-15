8a64214f0fa0c40bd52106359d514d3adc2b76b4
CMaucaylle <115099163+CintiaMQ@users.noreply.github.com>
2023-12-15 14:20:02 -0500

    Add files via upload

---
+++ my-app/src/mock/places.ts
@@ -0,0 +1,61 @@
+export const places = {
+    places: [
+      {
+        name: "Modern Art Loft",
+        rating: 4.5,
+        location: { country: "United States", city: "New York" },
+        image_url:
+          "https://cdn.tiqets.com/wordpress/blog/wp-content/uploads/2017/08/03134557/24-hours-in-new-york-1-1024x570.jpg",
+        date: "2023-01-15",
+        price: 120.99,
+        latitude: 40.712776,
+        longitude: -74.005974,
+      },
+      {
+        name: "Adventure National Park Lodge",
+        rating: 4.2,
+        location: { country: "Canada", city: "Vancouver" },
+        image_url:
+          "https://www.vmcdn.ca/f/files/via/images/city-images/downtown-vancouver-sunny-day-shore.jpg;w=960;h=586;mode=crop",
+        date: "2023-02-20",
+        price: 95.75,
+        latitude: 49.2827,
+        longitude: -123.1207,
+      },
+      {
+        name: "Seafront Restaurant Retreat",
+        rating: 4.8,
+        location: { country: "United Kingdom", city: "London" },
+        image_url:
+          "https://plus.unsplash.com/premium_photo-1661964203376-9aeba8cdbd23?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9uZG9uJTJDJTIwdW5pdGVkJTIwa2luZ2RvbXxlbnwwfHwwfHx8MA%3D%3D",
+        date: "2023-03-10",
+        price: 225.5,
+        latitude: 51.5074,
+        longitude: -0.1278,
+      },
+      {
+        name: "Mountain Trail Cabin",
+        rating: 4,
+        location: { country: "Australia", city: "Sydney" },
+        image_url:
+          "https://images.squarespace-cdn.com/content/v1/55ee34aae4b0bf70212ada4c/1577545161018-1F9Z9ZZQG9JO2O4WCWQX/keith-zhu-qaNcz43MeY8-unsplash+%281%29.jpg?format=1500w",
+        date: "2023-04-05",
+        price: 75.99,
+        latitude: -33.8688,
+        longitude: 151.2093,
+      },
+      {
+        name: "Downtown Theater Studio",
+        rating: 4.7,
+        location: { country: "United States", city: "Los Angeles" },
+        image_url:
+          "https://cdn.britannica.com/97/100097-050-7E411D6F/Harbor-Freeway-Los-Angeles.jpg",
+        date: "2023-05-12",
+        price: 150.25,
+        latitude: 34.0522,
+        longitude: -118.2437,
+      },
+      // ... (otros lugares con coordenadas actualizadas)
+      // Completa los restantes con coordenadas aproximadas o específicas si las tienes
+    ],
+  };
